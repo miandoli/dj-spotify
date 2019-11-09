8dbfe73979d51fcf8aa705c1b5c1998169735282
miandoli <miandoli8@gmail.com>
2019-11-09 13:02:39 -0500

    Created basic blade files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Mike is a hot piece of ass :)</h1>
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/app.css" rel="stylesheet"/>
+    <script src="/js/app.js"></script>
+</head>
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+</body>
