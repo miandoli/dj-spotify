e02e3ee70df5a01ce27b606f67ba3ab5e6d5b583
miandoli <miandoli8@gmail.com>
2019-11-09 17:44:13 -0500

    Merge fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,13 @@
     <script src="{{ asset('js/join.js') }}" defer></script>
     <script src="{{ asset('js/all.js') }}" defer></script>
     <script src="{{ asset('js/party.js') }}" defer></script>
+    <script src="{{ asset('js/join.js') }}" defer></script>
+    <script src="{{ asset('js/all.js') }}" defer></script>
+    <script src="{{ asset('js/party.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
